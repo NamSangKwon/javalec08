098ee37d074e0febf7b967e25e5f836e12bf1a10
남상권 <nsgk5720@gmail.com>
2023-08-24 21:52:08 +0900

    2일차 자바강의 commit (변수)

---
+++ Day02/src/Grammar.java
@@ -0,0 +1,29 @@
+/*public class Test {
+
+	public void 유효범위 () {
+		자바는 블록단위 문법을 사용합니다.
+	}
+	
+	public void 유효범위2 () {
+		
+	}
+}*/
+public class Grammar {
+
+	public static void main (String[] args) {
+		
+		//한 줄 주석
+		/*
+		 여
+		 러
+		 줄
+		 주
+		 석
+		 */
+		
+		
+		
+		
+		System.out.println("문법은 Grammar야.");
+	}
+}
---
+++ Day02/src/Grammar2.java
@@ -0,0 +1,8 @@
+
+public class Grammar2 {
+
+	public static void main(String[] args) {
+	// 메서드( = 함수) = 메서드 블록
+		
+	}
+}
